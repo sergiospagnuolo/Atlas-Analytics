a29f2ff2c663cc7ac5da60b6e4d56384c8fcfa2c
voltdatalab <sergio@voltdata.info>
2017-10-16 15:18:09 -0200

    adiciona analises uf

--- db/analises/merge_indicadores_cidades_atlas.sql
+++ db/analises/merge_indicadores_cidades_atlas.sql
@@ -1,5 +1,4 @@
 select *
 from jornais_cidades
-inner join idhm_cid
-on (jornais_cidades.cidade=idhm_cid.cidade and jornais_cidades.uf=idhm_cid.uf)
+natural join idhm_cid
 order by "count(atlas.cidade)" DESC
