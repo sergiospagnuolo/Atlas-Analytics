2a6dc26ec00e73310e68991628f15f67b7e0df23
rmhirota <32062127+rmhirota@users.noreply.github.com>
2018-06-20 18:07:18 -0300

    add view atlasrtv_slim_cidade_100khab_idhm to db

--- db/sql/count_cidades_radio+tv.sql
+++ db/sql/count_cidades_radio+tv.sql
@@ -1,4 +1,5 @@
-select count(id) as veiculos_mapeados, cidade, regiao_metropolitana, uf,
-estado, regiao, pais from atlas_radiofrequencia
+select cidade, uf, estado, count(id) as veiculos_mapeados, count(id)/POP*100000 as vei100milh, 
+regiao, pais, POP, RDPC, I_ESCOLARIDADE, IDHM, IDHM_E, IDHM_L, IDHM_R 
+from atlasrtv_completo_idhm_cidades
 group by uf, cidade
 order by veiculos_mapeados desc
