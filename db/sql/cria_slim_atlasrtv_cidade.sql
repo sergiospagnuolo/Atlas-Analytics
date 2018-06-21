e5aa8aa50f4977f9df57995941188c061ac2f145
rmhirota <32062127+rmhirota@users.noreply.github.com>
2018-06-21 15:58:20 -0300

    add sql cria_slim_atlasrtv

---
+++ db/sql/cria_slim_atlasrtv_cidade.sql
@@ -0,0 +1,9 @@
+select cidade, regiao_metropolitana, uf, regiao, veiculos_mapeados,
+veiculos_mapeados/POP*100000 as vei100milh, regiao, pais, POP, IDHM,
+I_ESCOLARIDADE, IDHM_E, IDHM_L, IDHM_R, RDPC from
+(select * from (select *, count(id) as veiculos_mapeados from atlas_radiofrequencia
+where meio = "Televisão"
+group by cidade, uf)
+natural join idhm_municipal_2010)
+
+order by veiculos_mapeados desc
