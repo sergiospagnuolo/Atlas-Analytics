5d17278fc6c5e56e915b6767db14fcfb5b0e0a34
rmhirota <32062127+rmhirota@users.noreply.github.com>
2018-06-22 17:16:46 -0300

    update views rm, uf in atlas.db

--- db/sql/cria_slim_atlastotal_cidade.sql
+++ db/sql/cria_slim_atlastotal_cidade.sql
@@ -1,5 +1,5 @@
 select cidade, regiao_metropolitana, uf, regiao, veiculos_mapeados,
-veiculos_mapeados/POP*100000 as vei100milh, regiao, pais, POP, IDHM,
+veiculos_mapeados/POP*100000 as vei100milh, pais, POP, IDHM,
 I_ESCOLARIDADE, IDHM_E, IDHM_L, IDHM_R, RDPC from
 (select * from 
 	(select *, count(id) as veiculos_mapeados from 
