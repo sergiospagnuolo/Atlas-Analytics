537b3a3474c5eae3f2f267480345b5db33e659e8
renatamh <re.hirota@gmail.com>
2018-06-30 17:28:10 -0300

    update views cidade, uf

--- db/sql/cria_slim_atlasrtv_cidade.sql
+++ db/sql/cria_slim_atlasrtv_cidade.sql
@@ -1,4 +1,4 @@
-select cidade, regiao_metropolitana, uf, regiao, veiculos_mapeados,
+select cast(Codmun6 as integer) as Codmun6, cast(Codmun7 as integer) as Codmun7, cidade, regiao_metropolitana, uf, regiao, veiculos_mapeados,
 veiculos_mapeados/POP*100000 as vei100milh, pais, POP, IDHM,
 I_ESCOLARIDADE, IDHM_E, IDHM_L, IDHM_R, RDPC from
 (select * from (select *, count(id) as veiculos_mapeados from atlas_radiofrequencia
