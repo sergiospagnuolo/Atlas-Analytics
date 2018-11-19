81e0f8cdbf6c9d5ad4c1f85d01da83f6598d9ef6
renatamh <re.hirota@gmail.com>
2018-11-19 12:30:14 -0200

    add prop veiculo/100khab to view atlas1e2_cidades

--- atlas_fase2/sql/analise_por_cidade.sql
+++ atlas_fase2/sql/analise_por_cidade.sql
@@ -1,4 +1,5 @@
-select a.cidade, a.uf, count(*) veiculos, b.codmun, b.pop_dou_2017 
+select a.cidade, a.uf, count(*) veiculos, b.codmun, b.pop_dou_2017, count(*)*100000/b.pop_dou_2017 veiculos_por_100khab
 from atlas1e2 a join ibge_pop b
 on a.cidade = b.cidade and a.uf = b.uf
 group by a.cidade, a.uf
+order by a.cidade
